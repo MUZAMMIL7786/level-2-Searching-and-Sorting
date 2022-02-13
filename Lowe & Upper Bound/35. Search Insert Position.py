d747b5b52250e97a3e4bb96f744031a91bc9efc4
MUZAMMIL7786 <77747853+MUZAMMIL7786@users.noreply.github.com>
2022-02-13 22:05:23 +0530

    Create 35. Search Insert Position.py

---
+++ Lowe & Upper Bound/35. Search Insert Position.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/search-insert-position/
+
+class Solution:
+    def searchInsert(self, arr: List[int], target: int) -> int:
+        left = 0; right = len(arr)-1
+        while(left <= right):
+            mid = (left+right)//2
+            
+            if(arr[mid] == target):
+                return mid
+            elif(arr[mid] < target):
+                res = mid+1
+                left = mid+1
+            else:
+                res = mid
+                right = mid-1
+        return res
+                
+            
