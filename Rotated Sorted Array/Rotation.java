1ea4c298c222ac454993a53cab95256f5eb7ea87
MUZAMMIL7786 <77747853+MUZAMMIL7786@users.noreply.github.com>
2022-02-13 21:54:50 +0530

    Create Rotation.java

---
+++ Rotated Sorted Array/Rotation.java
@@ -0,0 +1,15 @@
+class Solution {
+    int find(int arr[], int left, int right){
+        if(left == right) return left;
+        int mid = (left+right)/2;
+        if(arr[mid] < arr[right]) return find(arr, left, mid);
+        if(arr[mid] > arr[right]) return find(arr, mid+1, right);
+        
+        return left;
+    }
+    
+    int findKRotation(int arr[], int n) {
+        // code here
+        return(find(arr, 0, arr.length-1));
+    }
+}
